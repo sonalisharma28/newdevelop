8cd4bec125eb8f6bacbeda2921145658371d849b
Sonali <Sonali@DESKTOP-BGREQLU>
2019-03-11 23:01:44 +0530

    changes

--- DemoProject/src/test/DemoClass.java
+++ DemoProject/src/test/DemoClass.java
@@ -3,9 +3,9 @@ package test;
 public class DemoClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("changes in master");
-		
 		System.out.println("Hi");
+		
+		System.out.println("changes in master");
 		System.out.println("to practice conflicts");
 	}
 }
